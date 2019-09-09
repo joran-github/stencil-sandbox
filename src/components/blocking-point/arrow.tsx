cfd23ec9571b79212a6c956b31d7f720621a7fa7
THOMAS Samuel (ELONET) <samuel.thomas@ext.vallourec.com>
2019-09-09 16:49:15 +0200

    first commit

---
+++ src/components/blocking-point/arrow.tsx
@@ -0,0 +1,122 @@
+import { Component, h, Listen, State, Prop, Element, getAssetPath } from '@stencil/core';
+import $ from "jquery";
+window['jQuery'] = $;
+import 'bootstrap';
+
+
+@Component({
+  tag: 'arrow-component',
+  styleUrl: 'arrow.scss',
+  assetsDirs: ['assets']
+})
+export class Arrow {
+
+  @Prop() arrowId: string;
+
+  @Prop() visible: boolean = true;
+
+  @Prop() arrowImage: string = 'arrow-red.png';
+
+  @Prop() RedArrowImage: string = 'arrow-green.png';
+
+  @Prop() UndefinedArrowImage: string = 'arrow-grey.png';
+
+  @Prop() orientation?: 'top' | 'bottom' | 'right' | 'left';
+
+  @Prop() reasonsTrigger?: 'click' | 'hover' | 'focus' | 'manual';
+
+  @Prop() reasonsPosition?: 'top' | 'bottom' | 'right' | 'left' | 'auto' = 'auto';
+
+  @State() arrowState?: 'undefined' | 'blocking' | 'permit' = 'undefined';
+
+  @Element() arrow: HTMLElement;
+
+
+  componentDidLoad() {
+    this.updateStyle();
+  }
+
+  componentWillUpdate() {
+    this.updateStyle();
+  }
+
+
+  private updateStyle() {
+    // Get blocking point  content
+    let arrowContent = this.arrow.getElementsByClassName("arrow-point");
+
+    arrowContent[0].setAttribute("style", "background-image: url('./assets/imgs/this.arrowImage')" );
+  }
+
+  /**
+   * Get the component class
+   */
+  private getOrientationClass() {
+
+    let orientationClass: string;
+
+    // Determine class
+    switch (this.orientation) {
+      case 'top':
+          orientationClass = 'rotate-up';
+          break;
+      case 'bottom':
+          orientationClass = 'rotate-down';
+          break;
+      case 'right':
+          orientationClass = 'rotate-right';
+          break;
+      case 'left':
+          orientationClass = 'rotate-left';
+          break;
+      default:
+          orientationClass = '';
+          break;
+    }
+
+    return orientationClass;
+  }
+
+    /**
+   * Get blocking point visibility
+   */
+  private getVisibilityClass() {
+    return this.visible?"visible":"hidden";
+  }
+
+  /**
+   * Get blocking point state
+   */
+  private getStateClass( ) {
+    let arrowStateClassName;
+
+    switch (this.arrowState) {
+      case 'permit':
+        arrowStateClassName = 'state-not-blocking';
+        break;
+      case 'blocking':
+        arrowStateClassName = 'state-blocking';
+        break;
+      default:
+        arrowStateClassName = 'state-undefined';
+          break;
+    }
+
+    return arrowStateClassName;
+  }
+
+  private getArrowClass() {
+    let arowClass = 'blocking-point';
+    arowClass += ' ' + this.getVisibilityClass();
+    arowClass += ' ' + this.getStateClass();
+    arowClass += ' ' + this.getOrientationClass();
+    return arowClass;
+  }
+
+  render() {
+    return (
+          <div id={this.arrowId} class={this.getArrowClass()} >
+          </div>
+    );
+  }
+}
---
+++ stencil.config.ts
@@ -0,0 +1,29 @@
+import { Config } from '@stencil/core';
+import { sass } from '@stencil/sass';
+
+export const config: Config = {
+  namespace: 'arrow-component',
+  testing: {
+    /**
+     * Gitlab CI doesn't allow sandbox, therefor this parameters must be passed to your Headless Chrome
+     * before it can run your tests
+     */
+    browserArgs: ['--no-sandbox', '--disable-setuid-sandbox'],
+  },
+  outputTargets: [
+    {
+      type: 'dist',
+      esmLoaderPath: '../loader'
+    },
+    {
+      type: 'docs-readme'
+    },
+    {
+      type: 'www',
+      serviceWorker: null, // disable service workers
+    }
+  ],
+  plugins: [
+    sass()
+  ]
+};
